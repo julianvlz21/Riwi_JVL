399d3ea2eba9fa087f543e32ea55a7dc8634115f
julianvlz21 <julianvluna11@gmail.com>
2026-03-13 20:39:34 -0500

    advance_4

--- m1s1.py
+++ m1s1.py
@@ -1,3 +1,8 @@
+##Inventario de tienda
+
+#Se usa ciclo "while" con "True" para solicitar los datos de cada usuario, también se metió este bloque dentro de un 
+# "try" y "except" en caso que lleguen a ingresar un valor erróneo.
+
 nombre = str(input("\nEscribe el nombre del productos: "))
 
 while True:
@@ -6,13 +11,17 @@ while True:
         cantidad = int(input("Escribe la cantidad de productos: "))
 
         if precio < 0 or cantidad <= 0:
-            print("El valor no puede ser 0 o negaitivo") 
-            continue
+            print("El valor no puede ser 0 o negaitivo")
     except:
         print("\n¡¡¡Valor invalido, intenta de nuenvo!!!")
-        
-    costo_total = precio * cantidad
-    print(f"\nProducto: {nombre}")
-    print(f"Precio unitario: ${precio} | Cantidad: {cantidad} | Costo total: ${costo_total}")   
+#Se crea una variable que guarde el "costo_total" de la multiplicación del "precio * cantidad" y se imprimen los valores
+# para que el usuario los vea la información del producto de manera organizada.
+      
+    try:  
+        costo_total = precio * cantidad
+        print(f"\nProducto: {nombre}")
+        print(f"Precio unitario: ${precio} | Cantidad: {cantidad} | Costo total: ${costo_total}")   
     except NameError:
-    print ("")
+        print ("")
+
+#Este scrip, tiene como finalidad registrar los datos de productos para un inventario, proporcionar costos y validar errores.
