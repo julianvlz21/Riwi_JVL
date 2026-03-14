023438eb080cb3c1a0c5fbb6dba66649ee20b342
julianvlz21 <julianvluna11@gmail.com>
2026-03-13 19:44:01 -0500

    advance_1

---
+++ m1s1.py
@@ -0,0 +1,13 @@
+nombre = str(input("\nEscribe el nombre del productos: "))
+
+while True:
+    try:
+        precio = float(input("Agrega un precio al producto: "))
+        cantidad = int(input("Escribe la cantidad de productos: "))
+
+        if precio < 0 or cantidad <= 0:
+            print("El valor no puede ser 0 o negaitivo") 
+            continue
+    except:
+        print("\n¡¡¡Valor invalido, intenta de nuenvo!!!")
+        
