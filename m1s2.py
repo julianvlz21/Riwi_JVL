b7fcd82d7ffe2a687b3a9c37d97183bc3a6a94a7
Mrcristian1102 <mrcristian1103@gmail.com>
2026-03-26 07:08:46 -0500

    aplicación_funciones_validación_errores

--- m1s2.py
+++ m1s2.py
@@ -9,7 +9,7 @@ def agregar_productos(id, nombre, precio, cantidad):#Se crea un diccioanrio que 
     }
 
     inventario.append(producto)
-    print("\n\033[1;32m===============¡¡¡Registro exitoso!!!===============\033[0")
+    print("\n\033[1;32m===============¡¡¡Registro exitoso!!!===============\033[0m")
 
 def mostrar_inventario(inventario):
     if not inventario:
@@ -26,3 +26,77 @@ def calcular_estadisticas(inventario):
     print (total_inventario)
 
 #Validaciones
+def validar_datos():
+    while True:
+        nombre = input("Nombre: ")
+        precio = float(input("Precio: "))
+        cantidad = int(input("Cantidad: "))
+        try:
+            if nombre == "" or precio <= 0 or cantidad <= 0:
+                print("Datos erroneos, intente de nuevo")
+                continue
+            break
+        except:
+            ("\n\033[1,31m¡¡¡Valor invalido, intenta de nuenvo!!!\033[0m")
+        return nombre, precio, cantidad
+        
+        
+
+
+#menú para usuario
+while True:
+    print("\n\033[1;34m------------------------MENÚ REGISTRO INVENTARIO------------------------\033[0m")
+    print("""
+    \033[1;34m 1.\033[0m Agregar producto
+    \033[1;34m 2.\033[0m Mostrar inventario
+    \033[1;34m 3.\033[0m Calcular estadísticas
+    \033[1;34m 4.\033[0m Salir
+
+""")    
+    opcion = input("Selecciona una de las opciones: ")
+
+    if opcion == "1":
+        validar_datos
+
+    elif opcion == "2":
+        mostrar_inventario(inventario)
+
+
+
+
+# def si_0_negativo(valor):
+
+#     return int(valor) <= 0
+
+# def si_vacio (valor):
+
+#     return valor == str("")
+
+    # try:
+    #     opcion = input("Seleccione una opción: ")
+    #     if opcion == "1":
+    #         print("\033[32m CAPACIDAD PARA 20 PRODUCTOS\033[0m") 
+    #         cantidad = input("\nCuántos producto va a agregar: ")
+    #         if si_0_negativo(cantidad) or si_vacio(cantidad):
+    #             print("Valor invalido")
+    #         elif int(cantidad) > 20:
+    #             print("\033[32m CAPACIDAD PARA 20 PRODUCTOS\033[0m") 
+    #         else:
+    #             for n in range(int(cantidad)):
+    #                 while True:
+    #                     nombre = input("\nEscribe el nombre del productos: ")
+    #                     if si_vacio(nombre):
+    #                         print("\033[34m NOMBRE OBLIGATORIO\033[0m")
+    #                     else:
+    #                         precio = float(input("Agrega un precio al producto: "))
+    #                         cantidad_pro= int(input("Escribe la cantidad de productos: "))
+    #                         contador += 1
+
+    #                         if si_0_negativo(precio) or si_0_negativo(cantidad_pro):
+    #                             print("\n\033[1;31m =====!!!El valor no puede ser 0 o negaitivo¡¡¡=====\033[0m")
+    #                         else:    
+    #                             break
+    #                 agregar_productos(contador,nombre,precio,cantidad)
+
+    # except:
+    #     print("\n¡¡¡Valor invalido, intenta de nuenvo!!!")
